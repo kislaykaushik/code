723165f71b9533b2cd73cb8965ba43ede6182eb1
Vicky-2000 <56164489+Vicky-2000@users.noreply.github.com>
2019-10-04 20:55:51 +0530

    Add files via upload

---
+++ strong.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{int d,i,x,n,fac,sum=0;
+printf("enter the number=");
+scanf("%d",&x);
+n=x;
+while(n>0)
+{d=n%10;
+fac=1;
+for(i=d;i>0;i--)
+{
+fac=fac*i;
+}sum=sum+fac;
+n=n/10;
+}
+printf("%d\n",sum);
+if(sum==x)
+{printf("number is strong");
+}
+else
+{printf("number is not strong");
+}
+return 0;
+}
